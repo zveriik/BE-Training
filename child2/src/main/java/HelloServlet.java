cf1a244eaa70927a08a1f8951f2568c01efb8bcb
Aleksey Zverkov <aleksey_zverkov@epam.com>
2017-02-15 15:24:32 +0300

    add servlet

---
+++ child2/src/main/java/HelloServlet.java
@@ -0,0 +1,38 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    public void init() throws ServletException {
+        // Servlet initialization code here
+        super.init();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        // Set response content type
+        response.setContentType("text/html");
+
+        // Actual logic goes here.
+        PrintWriter out = response.getWriter();
+        out.println("<h1>Hello Servlet</h1>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        super.doPost(request, response);
+    }
+
+    @Override
+    public void destroy() {
+        // resource release
+        super.destroy();
+    }
+}
