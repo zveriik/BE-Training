079f50e3f7fc8b6386ad805783a72e900161011c
Aleksey Zverkov <aleksey_zverkov@epam.com>
2017-05-10 17:21:45 +0300

    add MockTest

---
+++ src/test/java/DogControllerMockTest.java
@@ -0,0 +1,36 @@
+import com.controller.DogController;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/dog-test-servlet.xml")
+public class DogControllerMockTest {
+
+    private MockMvc mockMvc;
+    private DogController dogController = new DogController();
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        this.mockMvc = MockMvcBuilders
+                .standaloneSetup(dogController).build();
+    }
+
+    @Test
+    public void testGetDog() throws Exception {
+        this.mockMvc.perform(get("/dog"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE));
+    }
+}
